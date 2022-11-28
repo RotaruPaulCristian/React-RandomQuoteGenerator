9167c59d05355106e790791178c82ef50b16c8f7
RotaruPaulCristian <cristian.rotaru96@yahoo.com>
2022-11-29 01:12:34 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+// Importing useState from react
+import React, { useState, useEffect } from "react";
+import "./index.css";
 
-function App() {
+const App = () => {
+  const [advice, setAdvice] = useState("");
+
+  const fetchAdvice = () => {
+    const slipID = Math.floor(Math.random() * 100) + 1;
+    const adviceAPI = `https://api.adviceslip.com/advice/${slipID}`;
+
+    // Fetch a random advice slip
+    fetch(adviceAPI)
+      .then((response) => response.json())
+      .then((data) => {
+        // destructuring the advice from data
+        const { advice } = data.slip;
+
+        // update to new advice we just fetched
+        setAdvice(advice);
+      });
+  };
+
+  useEffect(() => fetchAdvice(), []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="card">
+        <h1 className="heading">{advice}</h1>
+        {/* Trigger fetchAdvice on button click */}
+        <button className="button" onClick={() => fetchAdvice()}>
+          <span>give me advice!</span>
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
